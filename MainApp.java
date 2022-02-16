f1c76561fa3de7637518beeca512ce7c86dc900f
alemenfon <alemenfon1@gmail.com>
2022-02-16 10:46:04 +0100

    Clase actualizada

---
+++ MainApp.java
@@ -0,0 +1,5 @@
+public static main (Strings[] args){
+
+
+
+}
---
+++ MainJava.java
@@ -0,0 +1,5 @@
+public class MainApp{
+Puiblic static void main (String[] args{
+	system.out.println("Primera subida a repositorio");
+	}
+}
